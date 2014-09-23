b4a2b03dd9feb169fa53beaf0662117a6c056a22
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-09-23 01:28:29 -0700

    add example for DupFixed/DupSort

--- example_test.go
+++ example_test.go
@@ -1,12 +1,126 @@
 package lmdb_test
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"strings"
 
 	"github.com/bmatsuo/lmdb.exp"
 )
+
+// This example demonstrates the DupFixed (and DupSort) DBI flags.
+func Example_dupFixed() {
+	env, err := lmdb.NewEnv()
+	if err != nil {
+		log.Panic(err)
+	}
+	path, err := ioutil.TempDir("", "mdb_test")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer os.RemoveAll(path)
+	err = env.SetMaxDBs(1)
+	if err != nil {
+		log.Panic(err)
+	}
+	err = env.Open(path, 0, 0644)
+	defer env.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// open the database of friends' phone numbers.  a single person can have
+	// multiple phone numbers and in this limited world phone nubers are all
+	// the same length.
+	txn, err := env.BeginTxn(nil, 0)
+	if err != nil {
+		log.Panic(err)
+	}
+	phonedb, err := txn.OpenDBI("phone-numbers", lmdb.Create|lmdb.DupSort|lmdb.DupFixed)
+	if err != nil {
+		txn.Abort()
+		log.Panic(err)
+	}
+	cur, err := txn.OpenCursor(phonedb)
+	if err != nil {
+		txn.Abort()
+		log.Panic(err)
+	}
+	entries := []struct{ name, number string }{
+		{"alice", "234-1234"},
+		{"bob", "825-1234"},
+		{"carol", "824-1234"},
+		{"carol", "502-1234"},
+		{"carol", "828-1234"}, // duplicate keys can be mixed and will be appended in the result
+		{"jenny", "867-5309"},
+	}
+	for _, e := range entries {
+		err = cur.Put([]byte(e.name), []byte(e.number), 0)
+		if err != nil {
+			txn.Abort()
+			log.Panic(err)
+		}
+	}
+	errc := cur.Close()
+	if err == nil {
+		err = errc
+	}
+	if err == nil {
+		err = txn.Commit()
+	} else {
+		txn.Abort()
+	}
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// iterate the database and print the first two phone numbers for each
+	// person.  this is similar to iterating a database normally but the
+	// NextNoDup flag may be used to skip ahead.
+	var lastk []byte
+	var isdup bool
+	txn, err = env.BeginTxn(nil, lmdb.Readonly)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer txn.Abort()
+	cur, err = txn.OpenCursor(phonedb)
+	if err != nil {
+		txn.Abort()
+		log.Panic(err)
+	}
+	k, v, err := cur.Get(nil, nil, lmdb.First)
+	for err != lmdb.ErrNotFound {
+		if err != nil {
+			log.Panic(err)
+		}
+		k, v, err = cur.Get(nil, nil, lmdb.GetCurrent)
+		if err != nil {
+			log.Panic(err)
+		}
+		isdup, lastk = bytes.Equal(lastk, k), k
+
+		if !isdup {
+			fmt.Printf("%s %s\n", k, v)
+			k, v, err = cur.Get(k, v, lmdb.Next)
+		} else {
+			// print space instead of the name because it's on the previous
+			// line and jump to the next key.
+			fmt.Printf("%s %s\n", strings.Repeat(" ", len(k)), v)
+			k, v, err = cur.Get(k, v, lmdb.NextNoDup)
+		}
+	}
+
+	// Output:
+	// alice 234-1234
+	// bob 825-1234
+	// carol 502-1234
+	//       824-1234
+	// jenny 867-5309
+}
 
 // This example shows how to use the Env type and open a database.
 func ExampleEnv() {
