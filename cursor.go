3fb65f55463b9d65423769a7524e2756b242f025
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-09-24 02:03:20 -0700

    duplicate Txn and Cursor as unsafeTxn and unsafeCursor

--- cursor.go
+++ cursor.go
@@ -170,3 +170,119 @@ func (cursor *Cursor) Count() (uint64, error) {
 	}
 	return uint64(_size), nil
 }
+
+// unsafeCursor operates on data inside a transaction and holds a position in the
+// database.
+//
+// See MDB_cursor.
+type unsafeCursor struct {
+	_cursor *C.MDB_cursor
+}
+
+func openUnsafeCursor(txn *unsafeTxn, db DBI) (*unsafeCursor, error) {
+	var _cursor *C.MDB_cursor
+	ret := C.mdb_cursor_open(txn._txn, C.MDB_dbi(db), &_cursor)
+	if ret != success {
+		return nil, errno(ret)
+	}
+	return &unsafeCursor{_cursor}, nil
+}
+
+// Renew associates readonly cursor with txn.
+//
+// See mdb_cursor_renew.
+func (cursor *unsafeCursor) Renew(txn *Txn) error {
+	ret := C.mdb_cursor_renew(txn._txn, cursor._cursor)
+	return errno(ret)
+}
+
+// Close the cursor handle.  The cursor must not be used after Close returns.
+// Cursors in write transactions must be closed before their transaction is
+// terminated.
+//
+// See mdb_cursor_close.
+func (cursor *unsafeCursor) Close() error {
+	if cursor._cursor == nil {
+		return errors.New("Cursor already closed")
+	}
+	C.mdb_cursor_close(cursor._cursor)
+	cursor._cursor = nil
+	return nil
+}
+
+// Txn returns the cursors transaction.
+//
+// See mdb_cursor_txn.
+func (cursor *unsafeCursor) Txn() *Txn {
+	var _txn *C.MDB_txn
+	_txn = C.mdb_cursor_txn(cursor._cursor)
+	if _txn != nil {
+		return &Txn{_txn}
+	}
+	return nil
+}
+
+// DBI returns the cursors database.
+func (cursor *unsafeCursor) DBI() DBI {
+	var _dbi C.MDB_dbi
+	_dbi = C.mdb_cursor_dbi(cursor._cursor)
+	return DBI(_dbi)
+}
+
+// MDBCursor retrieves the low-level MDB cursor.
+func (cursor *unsafeCursor) MDBCursor() *C.MDB_cursor {
+	return cursor._cursor
+}
+
+// Get retrieves items from the database. The slices returned by Get reference
+// readonly sections of memory and attempts to mutate the region of memory will
+// result in a panic.
+//
+// See mdb_cursor_get.
+func (cursor *unsafeCursor) Get(setkey, setval []byte, op uint) (key, val []byte, err error) {
+	k, v, err := cursor.GetVal(setkey, setval, op)
+	if err != nil {
+		return nil, nil, err
+	}
+	return k.Bytes(), v.Bytes(), nil
+}
+
+// GetVal retrieves items from the database.
+//
+// See mdb_cursor_get.
+func (cursor *unsafeCursor) GetVal(setkey, setval []byte, op uint) (key, val Val, err error) {
+	key = Wrap(setkey)
+	val = Wrap(setval)
+	ret := C.mdb_cursor_get(cursor._cursor, (*C.MDB_val)(&key), (*C.MDB_val)(&val), C.MDB_cursor_op(op))
+	return key, val, errno(ret)
+}
+
+// Put stores an item in the database.
+//
+// See mdb_cursor_put.
+func (cursor *unsafeCursor) Put(key, val []byte, flags uint) error {
+	ckey := Wrap(key)
+	cval := Wrap(val)
+	ret := C.mdb_cursor_put(cursor._cursor, (*C.MDB_val)(&ckey), (*C.MDB_val)(&cval), C.uint(flags))
+	return errno(ret)
+}
+
+// Del deletes the item referred to by the cursor from the database.
+//
+// See mdb_cursor_del.
+func (cursor *unsafeCursor) Del(flags uint) error {
+	ret := C.mdb_cursor_del(cursor._cursor, C.uint(flags))
+	return errno(ret)
+}
+
+// Count returns the number of duplicates for the current key.
+//
+// See mdb_cursor_count.
+func (cursor *unsafeCursor) Count() (uint64, error) {
+	var _size C.size_t
+	ret := C.mdb_cursor_count(cursor._cursor, &_size)
+	if ret != success {
+		return 0, errno(ret)
+	}
+	return uint64(_size), nil
+}
--- txn.go
+++ txn.go
@@ -194,3 +194,174 @@ func (txn *Txn) Del(dbi DBI, key, val []byte) error {
 func (txn *Txn) OpenCursor(dbi DBI) (*Cursor, error) {
 	return openCursor(txn, dbi)
 }
+
+// unsafeTxn is a database transaction in an environment.
+//
+// BUG: Using write transactions multiple goroutines has undefined results.
+//
+// See MDB_txn.
+type unsafeTxn struct {
+	_txn *C.MDB_txn
+}
+
+func beginUnsafeTxn(env *Env, parent *unsafeTxn, flags uint) (*unsafeTxn, error) {
+	var _txn *C.MDB_txn
+	var ptxn *C.MDB_txn
+	if parent == nil {
+		ptxn = nil
+	} else {
+		ptxn = parent._txn
+	}
+	if flags&Readonly == 0 {
+		runtime.LockOSThread()
+	}
+	ret := C.mdb_txn_begin(env._env, ptxn, C.uint(flags), &_txn)
+	if ret != success {
+		runtime.UnlockOSThread()
+		return nil, errno(ret)
+	}
+	return &unsafeTxn{_txn}, nil
+}
+
+// Commit commits all operations of the transaction to the database.
+//
+// See mdb_txn_commit.
+func (txn *unsafeTxn) Commit() error {
+	ret := C.mdb_txn_commit(txn._txn)
+	runtime.UnlockOSThread()
+	// The transaction handle is freed if there was no error
+	if ret == success {
+		txn._txn = nil
+	}
+	return errno(ret)
+}
+
+// Abort abandons operations of a transaction and does not persist them.
+//
+// See mdb_txn_abort.
+func (txn *unsafeTxn) Abort() {
+	if txn._txn == nil {
+		return
+	}
+	C.mdb_txn_abort(txn._txn)
+	runtime.UnlockOSThread()
+	// The transaction handle is always freed.
+	txn._txn = nil
+}
+
+// Reset aborts the transaction clears internal state so the transaction may be
+// reused by calling Renew.
+//
+// See mdb_txn_reset.
+func (txn *unsafeTxn) Reset() {
+	C.mdb_txn_reset(txn._txn)
+}
+
+// Renew reuses a transaction that was previously reset.
+//
+// See mdb_txn_renew.
+func (txn *unsafeTxn) Renew() error {
+	ret := C.mdb_txn_renew(txn._txn)
+	return errno(ret)
+}
+
+// Open opens a database in the environment.  If name is empty Open opens the
+// default database.
+//
+// TODO: Add an example showing potential problems using the default database.
+//
+// See mdb_dbi_open.
+func (txn *unsafeTxn) OpenDBI(name string, flags uint) (DBI, error) {
+	var _dbi C.MDB_dbi
+	var cname *C.char
+	if name != "" {
+		cname = C.CString(name)
+		defer C.free(unsafe.Pointer(cname))
+	}
+	ret := C.mdb_dbi_open(txn._txn, cname, C.uint(flags), &_dbi)
+	if ret != success {
+		return DBI(math.NaN()), errno(ret)
+	}
+	return DBI(_dbi), nil
+}
+
+// Stat returns statistics for database handle dbi.
+//
+// See mdb_stat.
+func (txn *unsafeTxn) Stat(dbi DBI) (*Stat, error) {
+	var _stat C.MDB_stat
+	ret := C.mdb_stat(txn._txn, C.MDB_dbi(dbi), &_stat)
+	if ret != success {
+		return nil, errno(ret)
+	}
+	stat := Stat{PSize: uint(_stat.ms_psize),
+		Depth:         uint(_stat.ms_depth),
+		BranchPages:   uint64(_stat.ms_branch_pages),
+		LeafPages:     uint64(_stat.ms_leaf_pages),
+		OverflowPages: uint64(_stat.ms_overflow_pages),
+		Entries:       uint64(_stat.ms_entries)}
+	return &stat, nil
+}
+
+// Drop empties the database if del is false.  Drop deletes and closes the
+// database if del is true.
+//
+// See mdb_drop.
+func (txn *unsafeTxn) Drop(dbi DBI, del bool) error {
+	ret := C.mdb_drop(txn._txn, C.MDB_dbi(dbi), cbool(del))
+	return errno(ret)
+}
+
+// Get retrieves items from database dbi.  The slice returned by Get references
+// a readonly section of memory and attempts to mutate region the memory will
+// result in a runtime panic.
+//
+// See mdb_get.
+func (txn *unsafeTxn) Get(dbi DBI, key []byte) ([]byte, error) {
+	val, err := txn.GetVal(dbi, key)
+	if err != nil {
+		return nil, err
+	}
+	return val.Bytes(), nil
+}
+
+// GetVal retrieves items from database dbi as a Val.
+//
+// See mdb_get.
+func (txn *unsafeTxn) GetVal(dbi DBI, key []byte) (Val, error) {
+	ckey := Wrap(key)
+	var cval Val
+	ret := C.mdb_get(txn._txn, C.MDB_dbi(dbi), (*C.MDB_val)(&ckey), (*C.MDB_val)(&cval))
+	return cval, errno(ret)
+}
+
+// Put stores an item in database dbi.
+//
+// See mdb_put.
+func (txn *unsafeTxn) Put(dbi DBI, key []byte, val []byte, flags uint) error {
+	ckey := Wrap(key)
+	cval := Wrap(val)
+	ret := C.mdb_put(txn._txn, C.MDB_dbi(dbi), (*C.MDB_val)(&ckey), (*C.MDB_val)(&cval), C.uint(flags))
+	return errno(ret)
+}
+
+// Del deletes items from database dbi.
+//
+// See mdb_del.
+func (txn *unsafeTxn) Del(dbi DBI, key, val []byte) error {
+	ckey := Wrap(key)
+	if val == nil {
+		ret := C.mdb_del(txn._txn, C.MDB_dbi(dbi), (*C.MDB_val)(&ckey), nil)
+		return errno(ret)
+	}
+	cval := Wrap(val)
+	ret := C.mdb_del(txn._txn, C.MDB_dbi(dbi), (*C.MDB_val)(&ckey), (*C.MDB_val)(&cval))
+	return errno(ret)
+}
+
+// OpenCursor allocates and initializes a Cursor to database dbi.
+//
+// See mdb_cursor_open.
+func (txn *unsafeTxn) OpenCursor(dbi DBI) (*unsafeCursor, error) {
+	return openUnsafeCursor(txn, dbi)
+}
