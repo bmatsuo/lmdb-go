4da89c9ee0c3f35f81712740970898c347b46e59
Bryan Matsuo <bryan.matsuo@gmail.com>
2015-10-09 00:13:30 -0700

    expand TestMultiVal

--- lmdb/val_test.go
+++ lmdb/val_test.go
@@ -1,15 +1,33 @@
 package lmdb
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 )
 
-func TestMultiVals(t *testing.T) {
-	m := WrapMulti([]byte("abc"), 1)
+func TestMultiVal(t *testing.T) {
+	data := []byte("abcdef")
+	m := WrapMulti(data, 2)
 	vals := m.Vals()
-	if !reflect.DeepEqual(vals, [][]byte{{'a'}, {'b'}, {'c'}}) {
+	if !reflect.DeepEqual(vals, [][]byte{{'a', 'b'}, {'c', 'd'}, {'e', 'f'}}) {
 		t.Errorf("unexpected vals: %q", vals)
+	}
+	size := m.Size()
+	if size != 6 {
+		t.Errorf("unexpected size: %v (!= %v)", size, 6)
+	}
+	length := m.Len()
+	if length != 3 {
+		t.Errorf("unexpected length: %v (!= %v)", length, 3)
+	}
+	stride := m.Stride()
+	if stride != 2 {
+		t.Errorf("unexpected stride: %v (!= %v)", stride, 2)
+	}
+	page := m.Page()
+	if !bytes.Equal(page, data) {
+		t.Errorf("unexpected page: %v (!= %v)", page, data)
 	}
 }
 
