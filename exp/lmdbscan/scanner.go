0503a0c4abf9bb9c16a0441fa0b86786094ef07e
Bryan Matsuo <bryan.matsuo@gmail.com>
2015-11-01 20:17:26 -0800

    Docs: typo

--- exp/lmdbscan/scanner.go
+++ exp/lmdbscan/scanner.go
@@ -40,7 +40,7 @@ func New(txn *lmdb.Txn, dbi lmdb.DBI) *Scanner {
 	return s
 }
 
-// Cursor returns the lmdb.Cursor underlying s.  Cursor returns nil if the s is
+// Cursor returns the lmdb.Cursor underlying s.  Cursor returns nil if s is
 // closed.
 func (s *Scanner) Cursor() *lmdb.Cursor {
 	return s.cur
