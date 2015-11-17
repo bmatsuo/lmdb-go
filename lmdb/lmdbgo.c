7125fed703adccd767ebedb83136cf249ad1264e
Bryan Matsuo <bryan.matsuo@gmail.com>
2015-11-16 19:25:23 -0800

    lmdb: clean up implementation of proxy functions using a macro
    The designated initializers used are specified in C99.  I'm not sure
    what the minimum requirement is for LMDB but this seems it seems
    reasonable for now.

--- lmdb/lmdbgo.c
+++ lmdb/lmdbgo.c
@@ -5,6 +5,9 @@
 #include "lmdbgo.h"
 #include "_cgo_export.h"
 
+#define LMDBGO_SET_VAL(val, size, data) \
+    *(val) = (MDB_val){.mv_size = (size), .mv_data = (data)}
+
 int lmdbgo_mdb_msg_func_proxy(const char *msg, void *ctx) {
     //  wrap msg and call the bridge function exported from lmdb.go.
     lmdbgo_ConstCString s;
@@ -20,73 +23,58 @@ int lmdbgo_mdb_reader_list(MDB_env *env, size_t ctx) {
 
 int lmdbgo_mdb_del(MDB_txn *txn, MDB_dbi dbi, void *kdata, size_t kn, void *vdata, size_t vn) {
     MDB_val key, val;
-    key.mv_size = kn;
-    key.mv_data = kdata;
-    val.mv_size = vn;
-    val.mv_data = vdata;
+    LMDBGO_SET_VAL(&key, kn, kdata);
+    LMDBGO_SET_VAL(&val, vn, vdata);
     return mdb_del(txn, dbi, &key, &val);
 }
 
 int lmdbgo_mdb_get(MDB_txn *txn, MDB_dbi dbi, void *kdata, size_t kn, MDB_val *val) {
     MDB_val key;
-    key.mv_size = kn;
-    key.mv_data = kdata;
+    LMDBGO_SET_VAL(&key, kn, kdata);
     return mdb_get(txn, dbi, &key, val);
 }
 
 int lmdbgo_mdb_put2(MDB_txn *txn, MDB_dbi dbi, void *kdata, size_t kn, void *vdata, size_t vn, unsigned int flags) {
     MDB_val key, val;
-    key.mv_size = kn;
-    key.mv_data = kdata;
-    val.mv_size = vn;
-    val.mv_data = vdata;
+    LMDBGO_SET_VAL(&key, kn, kdata);
+    LMDBGO_SET_VAL(&val, vn, vdata);
     return mdb_put(txn, dbi, &key, &val, flags);
 }
 
 int lmdbgo_mdb_put1(MDB_txn *txn, MDB_dbi dbi, void *kdata, size_t kn, MDB_val *val, unsigned int flags) {
     MDB_val key;
-    key.mv_size = kn;
-    key.mv_data = kdata;
+    LMDBGO_SET_VAL(&key, kn, kdata);
     return mdb_put(txn, dbi, &key, val, flags);
 }
 
 int lmdbgo_mdb_cursor_put2(MDB_cursor *cur, void *kdata, size_t kn, void *vdata, size_t vn, unsigned int flags) {
     MDB_val key, val;
-    key.mv_size = kn;
-    key.mv_data = kdata;
-    val.mv_size = vn;
-    val.mv_data = vdata;
+    LMDBGO_SET_VAL(&key, kn, kdata);
+    LMDBGO_SET_VAL(&val, vn, vdata);
     return mdb_cursor_put(cur, &key, &val, flags);
 }
 
 int lmdbgo_mdb_cursor_put1(MDB_cursor *cur, void *kdata, size_t kn, MDB_val *val, unsigned int flags) {
     MDB_val key;
-    key.mv_size = kn;
-    key.mv_data = kdata;
+    LMDBGO_SET_VAL(&key, kn, kdata);
     return mdb_cursor_put(cur, &key, val, flags);
 }
 
 int lmdbgo_mdb_cursor_putmulti(MDB_cursor *cur, void *kdata, size_t kn, void *vdata, size_t vn, size_t vstride, unsigned int flags) {
     MDB_val key, val[2];
-    key.mv_size = kn;
-    key.mv_data = kdata;
-    val[0].mv_size = vstride;
-    val[0].mv_data = vdata;
-	val[1].mv_data = 0;
-	val[1].mv_size = vn;
+    LMDBGO_SET_VAL(&key, kn, kdata);
+    LMDBGO_SET_VAL(&(val[0]), vstride, vdata);
+    LMDBGO_SET_VAL(&(val[1]), vn, 0);
     return mdb_cursor_put(cur, &key, &val[0], flags);
 }
 
 int lmdbgo_mdb_cursor_get1(MDB_cursor *cur, void *kdata, size_t kn, MDB_val *key, MDB_val *val, MDB_cursor_op op) {
-    key->mv_size = kn;
-    key->mv_data = kdata;
+    LMDBGO_SET_VAL(key, kn, kdata);
     return mdb_cursor_get(cur, key, val, op);
 }
 
 int lmdbgo_mdb_cursor_get2(MDB_cursor *cur, void *kdata, size_t kn, void *vdata, size_t vn, MDB_val *key, MDB_val *val, MDB_cursor_op op) {
-    key->mv_size = kn;
-    key->mv_data = kdata;
-    val->mv_size = vn;
-    val->mv_data = vdata;
+    LMDBGO_SET_VAL(key, kn, kdata);
+    LMDBGO_SET_VAL(val, vn, vdata);
     return mdb_cursor_get(cur, key, val, op);
 }
